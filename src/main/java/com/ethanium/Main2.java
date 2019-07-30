7217178415d0d64f3b45c2ec31a84385b3f9cf83
rollyn <rollyn.moises@gmail.com>
2019-07-30 22:08:00 +0800

    change in future

--- src/main/java/com/ethanium/Main2.java
+++ src/main/java/com/ethanium/Main2.java
@@ -3,8 +3,10 @@ package com.ethanium;
 import akka.actor.ActorRef;
 import akka.actor.ActorSystem;
 import akka.actor.Props;
+import akka.dispatch.OnSuccess;
 import akka.util.Timeout;
 import scala.concurrent.Await;
+import scala.concurrent.ExecutionContext;
 import scala.concurrent.Future;
 import scala.concurrent.duration.FiniteDuration;
 import scala.reflect.ClassTag$;
@@ -19,16 +21,30 @@ public class Main2 {
     public static void main(String[] args) throws Exception {
 
         ActorSystem system = ActorSystem.create();
-        ActorRef actorRef = system.actorOf(Props.create(Consumer2.class));
+        ActorRef actorRef = system.actorOf(Consumer2.props());
 
         actorRef.tell(new SayHello(), ActorRef.noSender());
         actorRef.tell(new SayBye(), ActorRef.noSender());
 
-        FiniteDuration duration = FiniteDuration.create(1, TimeUnit.SECONDS);
-        Timeout timeout = Timeout.durationToTimeout(duration);
-        Future<String> f = ask(actorRef, new Hola(), 1).mapTo(ClassTag$.MODULE$.apply(String.class));
-        String result = (String) Await.result(f, timeout.duration());
-        System.out.println("RETURNED::"+result);
+       // FiniteDuration duration = FiniteDuration.create(1, TimeUnit.SECONDS);
+        //Timeout timeout = Timeout.durationToTimeout(duration);
+       // Future<String> f = ask(actorRef, new Hola(), 1).mapTo(ClassTag$.MODULE$.apply(String.class));
+        //String result = (String) Await.result(f, timeout.duration());
+        //System.out.println("RETURNED::"+result);
+
+        Timeout timeout = new Timeout(3, TimeUnit.SECONDS);
+        Future<Object> future = ask(actorRef, new Hola(), timeout);
+
+        final ExecutionContext ec = system.dispatcher();
+        future.onSuccess(new OnSuccess<Object>() {
+
+            @Override
+            public void onSuccess(Object message) throws Throwable {
+                if (message instanceof String) {
+                    System.out.println("RETURNED MSG: "+message);
+                }
+            }
+        },ec);
 
         system.terminate();
     }
